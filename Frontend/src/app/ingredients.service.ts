a461928684704d4779a3db1a1c994c789a606fdd
Lina <c.hornhuber@students.htl-leonding.ac.at>
2025-01-04 22:51:25 +0100

    working on Ingredients page

---
+++ Frontend/src/app/ingredients.service.ts
@@ -0,0 +1,38 @@
+import {Injectable} from '@angular/core';
+
+export interface Ingredient  {
+  name:string,
+  slot:number,
+  remaining_liquid:number
+}
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IngredientsService {
+  constructor() { }
+  async getAllIngredients(): Promise<Ingredient[]> {
+    return [{
+      name: 'Gin',
+      slot: 0,
+      remaining_liquid: 200
+    }, {
+      name: 'Vodka',
+      slot: 1,
+      remaining_liquid: 200
+    }, {
+      name: 'Rakija',
+      slot: 2,
+      remaining_liquid: 200
+    }, {
+      name: 'Liqör',
+      slot: 3,
+      remaining_liquid: 200
+    }, {
+      name: 'Apfelkuchen',
+      slot: 4,
+      remaining_liquid: 200
+    }];
+  }
+}
--- Frontend/src/app/ingredients/ingredients.component.ts
+++ Frontend/src/app/ingredients/ingredients.component.ts
@@ -1,11 +1,55 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+  CdkDrag,
+  CdkDropList,
+} from '@angular/cdk/drag-drop';
+import {Ingredient, IngredientsService} from '../ingredients.service';
 
 @Component({
   selector: 'app-ingredients',
-  imports: [],
+  imports: [CdkDropList, CdkDrag],
   templateUrl: './ingredients.component.html',
+  standalone: true,
   styleUrl: './ingredients.component.css'
 })
 export class IngredientsComponent {
+  private readonly ingredientsService = inject(IngredientsService);
 
+
+  availableIngredients: Ingredient[]=[];
+  notAvailableIngredients: Ingredient[]=[];
+
+  async ngOnInit() {
+    let allIngredients: Ingredient[] = await this.ingredientsService.getAllIngredients();
+    allIngredients.forEach(ingredient => {
+      ingredient.slot===0 ?
+        this.notAvailableIngredients.push(ingredient)
+        : this.availableIngredients.push(ingredient);
+    });
+  }
+
+
+  drop(event: CdkDragDrop<Ingredient[], any>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+    for (let i = 0; i < this.availableIngredients.length; i++) {
+     this.availableIngredients[i].slot=i+1;
+    }
+    for (let i = 0; i < this.notAvailableIngredients.length; i++) {
+      this.notAvailableIngredients[i].slot=0;
+    }
+    console.log(this.availableIngredients);
+    console.log(this.notAvailableIngredients);
+  }
 }
