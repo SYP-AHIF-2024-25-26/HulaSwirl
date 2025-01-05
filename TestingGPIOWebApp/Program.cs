32040756db5b1ffe883ac90bf1fa99da1639c151
Moritz Bernhofer <114606708+MoritzBernhofer@users.noreply.github.com>
2025-01-05 21:37:25 +0100

    add hosting url

--- TestingGPIOWebApp/Program.cs
+++ TestingGPIOWebApp/Program.cs
@@ -2,7 +2,11 @@
 using TestingGPIOWebApp;
 
 var builder = WebApplication.CreateBuilder(args);
+
+
 var app = builder.Build();
+
+app.Urls.Add("http://192.168.178.62:5000");
 
 var pump = new Pump(17, 27);
 
