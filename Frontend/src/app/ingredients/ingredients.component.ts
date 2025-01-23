14edc7cb6897bf9e9f44ac393a5b66012cac5580
Ley <leooberndorfer06@gmail.com>
2025-01-23 09:57:58 +0100

    Fixed some issues to make work easier

--- Frontend/src/app/ingredients/ingredients.component.ts
+++ Frontend/src/app/ingredients/ingredients.component.ts
@@ -1,11 +1,13 @@
 import {Component, Signal, signal} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {Ingredient} from '../ingredients.service';
+import {NgForOf} from '@angular/common';
 
 @Component({
-  selector: 'app-home',
+  selector: 'app-ingredients',
   imports: [
-    FormsModule
+    FormsModule,
+    NgForOf
   ],
   templateUrl: './ingredients.component.html',
   standalone: true,
