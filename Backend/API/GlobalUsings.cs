c90d614d983501c830e6ca5c4974c5520b8edcb6
Moritz Bernhofer <114606708+MoritzBernhofer@users.noreply.github.com>
2025-02-07 08:39:18 +0100

    remove old backend and move gpiopintesting project

--- Backend/API/GlobalUsings.cs
+++
@@ -1,9 +0,0 @@
-// Global using directives
-
-global using System.ComponentModel.DataAnnotations;
-global using System.ComponentModel.DataAnnotations.Schema;
-global using System.Device.Gpio;
-global using API.Model;
-global using API.Services;
-global using Microsoft.AspNetCore.Mvc;
-global using Microsoft.EntityFrameworkCore;
--- Backend/API/Model/Order.cs
+++
@@ -1,9 +0,0 @@
-namespace API.Model;
-
-public class Order {
-    [Key] public int Id { get; set; }
-
-    [ForeignKey(nameof(Drink))] public int DrinkId { get; set; }
-
-    public DateTime OrderDateTime { get; set; }
-}
