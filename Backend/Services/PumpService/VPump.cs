0876282c832a26b207629e70374b5e1d46d1f75b
Moritz Bernhofer <114606708+MoritzBernhofer@users.noreply.github.com>
2025-03-04 15:20:17 +0100

    update experimental feature

--- Backend/Services/PumpService/VPump.cs
+++ Backend/Services/PumpService/VPump.cs
@@ -7,22 +7,29 @@ namespace Backend.Services.PumpService
     public class VPump
     {
         private const int Frequency = 20_000;
-        private readonly SoftwarePwmChannel _channel1;
+        private readonly SoftwarePwmChannel _pwmChannel1;
+        private readonly SoftwarePwmChannel _pwmChannel2;
         private readonly GpioController _controller;
+        private readonly int _in1;
         private readonly int _in2;
         private bool _isForward = true;
 
         public VPump(int in1, int in2, GpioController controller)
         {
             _controller = controller;
+            _in1 = in1;
             _in2 = in2;
 
-            // Create PWM channel
-            _channel1 = new SoftwarePwmChannel(in1, Frequency, 0);
+            // Create PWM channels for both pins
+            _pwmChannel1 = new SoftwarePwmChannel(_in1, Frequency, 0);
+            _pwmChannel2 = new SoftwarePwmChannel(_in2, Frequency, 0);
 
-            // Setup in2 as an output pin
+            // Ensure both pins are set as outputs
+            _controller.OpenPin(_in1, PinMode.Output);
             _controller.OpenPin(_in2, PinMode.Output);
-            _controller.Write(_in2, PinValue.Low); // Default to Forward
+
+            // Default direction: Forward (in1 -> PWM, in2 -> LOW)
+            _controller.Write(_in2, PinValue.Low);
         }
 
         public void SetSpeed(int percentage)
@@ -30,23 +37,48 @@ namespace Backend.Services.PumpService
             if (percentage is < 0 or > 100)
                 throw new ArgumentOutOfRangeException(nameof(percentage), "Percentage must be between 0 and 100");
 
-            _channel1.DutyCycle = Math.Round(percentage / 100.0, 2);
+            double dutyCycle = Math.Round(percentage / 100.0, 2);
+
+            if (_isForward)
+            {
+                _pwmChannel1.DutyCycle = dutyCycle;
+                _pwmChannel2.DutyCycle = 0;
+            }
+            else
+            {
+                _pwmChannel1.DutyCycle = 0;
+                _pwmChannel2.DutyCycle = dutyCycle;
+            }
         }
 
         public void Start()
         {
-            _channel1.Start();
+            _pwmChannel1.Start();
+            _pwmChannel2.Start();
         }
 
         public void Stop()
         {
-            _channel1.Stop();
+            _pwmChannel1.Stop();
+            _pwmChannel2.Stop();
         }
 
         public void ChangeDirection()
         {
             _isForward = !_isForward;
-            _controller.Write(_in2, _isForward ? PinValue.Low : PinValue.High);
+
+            if (_isForward)
+            {
+                _pwmChannel1.Start();
+                _pwmChannel2.Stop();
+                _controller.Write(_in2, PinValue.Low);
+            }
+            else
+            {
+                _pwmChannel1.Stop();
+                _pwmChannel2.Start();
+                _controller.Write(_in1, PinValue.Low);
+            }
         }
     }
 }
