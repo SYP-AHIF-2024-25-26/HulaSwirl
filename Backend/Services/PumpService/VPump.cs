e27a4e8afd994bb56e38f80b4820ea8fbf2ed97b
Moritz Bernhofer <114606708+MoritzBernhofer@users.noreply.github.com>
2025-03-04 15:09:47 +0100

    add experimental changes for changing pumping direction, to be testet

--- Backend/Services/PumpService/VPump.cs
+++ Backend/Services/PumpService/VPump.cs
@@ -1,42 +1,52 @@
+using System;
+using System.Device.Gpio;
 using System.Device.Pwm.Drivers;
 
-namespace Backend.Services.PumpService;
-
-public class VPump
+namespace Backend.Services.PumpService
 {
-    private const int Frequency = 20_000;
-
-    private readonly SoftwarePwmChannel _channel1;
-    private readonly GpioController _controller;
-
-
-    public VPump(int in1, int in2, GpioController controller)
+    public class VPump
     {
-        _controller = controller;
+        private const int Frequency = 20_000;
+        private readonly SoftwarePwmChannel _channel1;
+        private readonly GpioController _controller;
+        private readonly int _in2;
+        private bool _isForward = true;
 
-        //create channels
-        _channel1 = new SoftwarePwmChannel(in1, Frequency, 0);
+        public VPump(int in1, int in2, GpioController controller)
+        {
+            _controller = controller;
+            _in2 = in2;
 
-        controller.OpenPin(in2, PinMode.Output);
-        controller.Write(in2, PinValue.Low);
+            // Create PWM channel
+            _channel1 = new SoftwarePwmChannel(in1, Frequency, 0);
 
-        //in1 -> PWM, in2 -> LOW ---> pump Forward
-    }
+            // Setup in2 as an output pin
+            _controller.OpenPin(_in2, PinMode.Output);
+            _controller.Write(_in2, PinValue.Low); // Default to Forward
+        }
 
-    public void SetSpeed(int percentage)
-    {
-        if (percentage is < 0 or > 100) throw new Exception("percentage format -> between 0 - 100");
+        public void SetSpeed(int percentage)
+        {
+            if (percentage is < 0 or > 100)
+                throw new ArgumentOutOfRangeException(nameof(percentage), "Percentage must be between 0 and 100");
 
-        _channel1.DutyCycle = Math.Round(percentage / 100.0, 2);
-    }
+            _channel1.DutyCycle = Math.Round(percentage / 100.0, 2);
+        }
 
-    public void Start()
-    {
-        _channel1.Start();
-    }
+        public void Start()
+        {
+            _channel1.Start();
+        }
 
-    public void Stop()
-    {
-        _channel1.Stop();
+        public void Stop()
+        {
+            _channel1.Stop();
+        }
+
+        public void ChangeDirection()
+        {
+            _isForward = !_isForward;
+            _controller.Write(_in2, _isForward ? PinValue.Low : PinValue.High);
+        }
     }
 }
