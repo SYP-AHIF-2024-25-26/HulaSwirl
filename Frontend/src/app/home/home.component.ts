e073c706d9fe440e5a61c10ec34f3b7e46a6619b
Ley <leooberndorfer06@gmail.com>
2025-03-10 19:18:17 +0100

    Made app (home) responsive and cleaned up some style and ts. This fixes #37, closes #36, closes #20

--- Frontend/src/app/home/home.component.ts
+++ Frontend/src/app/home/home.component.ts
@@ -1,13 +1,9 @@
 import {Component, effect, ElementRef, inject, Signal, signal, ViewChild, WritableSignal} from '@angular/core';
 import {NgForOf, NgIf, NgOptimizedImage} from '@angular/common';
 import {Ingredient, IngredientsService} from '../services/ingredients.service';
-import {firstValueFrom, single} from 'rxjs';
 import {FormsModule} from '@angular/forms';
 import {Drink, DrinkService} from '../services/drink.service';
-import {OrderCustomDrinkModalComponent} from '../modals/order-custom-drink-modal/order-custom-drink-modal.component';
-import {OrderDrinkModalComponent} from '../modals/order-drink-modal/order-drink-modal.component';
 import {ModalService, ModalType} from '../services/modal.service';
-import {BackgroundLeavesComponent} from '../background-leaves/background-leaves.component';
 
 @Component({
   selector: 'app-home',
@@ -24,19 +20,19 @@ export class HomeComponent {
   private readonly drinkService = inject(DrinkService);
   private readonly modalService = inject(ModalService);
 
+  allDrinks = signal<Drink[]>([]);
   allAvailableIngredients = signal<Ingredient[]>([]);
 
-  K_bestDrinks = signal<Drink[]>([]);
-  D_allDrinks = signal<Drink[]>([]);
-  filteredDrinks = signal<Drink[]>(this.D_allDrinks());
-  K_currentSlide = signal(1);
+  recommendedDrinks = signal<Drink[]>([]);
+  filteredDrinks = signal<Drink[]>(this.allDrinks());
+  currentSlideIdx = signal(1);
   searchQuery: string = '';
   selectedIngredient: string = '';
 
   async ngOnInit() {
     this.allAvailableIngredients.set((await this.ingredientService.getAllIngredients()).filter(ing => ing.slot !== null));
-    this.K_bestDrinks.set((await this.drinkService.getDrinks()).slice(0, 5));
-    this.D_allDrinks.set(await this.drinkService.getDrinks());
+    this.recommendedDrinks.set((await this.drinkService.getDrinks()).slice(0, 5));
+    this.allDrinks.set(await this.drinkService.getDrinks());
     this.filteredDrinks.set(await this.drinkService.getDrinks());
   }
 
@@ -49,52 +45,56 @@ export class HomeComponent {
     this.targetElement.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'start',alignToTop:true });
   }
 
-  K_nextSlide(): void {
-    if (this.K_currentSlide() < this.K_bestDrinks().length - 1) {
-      this.K_currentSlide.set(this.K_currentSlide() + 1);
+  nextSlide(): void {
+    if(this.currentSlideIdx() < this.recommendedDrinks().length - 1) {
+      this.currentSlideIdx.set(this.currentSlideIdx() + 1);
     }
   }
-  K_prevSlide(): void {
-    if (this.K_currentSlide() > 0) {
-      this.K_currentSlide.set(this.K_currentSlide() - 1);
+  prevSlide(): void {
+    if (this.currentSlideIdx() > 0) {
+      this.currentSlideIdx.set(this.currentSlideIdx() - 1);
     }
-  }
-  K_getSlidePosition(index: number): string {
-    const totalSlides = this.K_bestDrinks().length;
-    const previousIndex = (this.K_currentSlide() - 1 + totalSlides) % totalSlides;
-    const nextIndex = (this.K_currentSlide() + 1) % totalSlides;
-    if (index === this.K_currentSlide()) return 'center';
-    if (index === previousIndex || index === nextIndex) return 'side';
-    return 'hidden'; // All others
-  }
-  K_isPrevDisabled(): boolean {
-    return this.K_currentSlide() === 0;
-  }
-  K_isNextDisabled(): boolean {
-    return this.K_currentSlide() === this.K_bestDrinks().length - 1;
   }
 
-  D_searchDrinks() {
+  getSlideState(index: number): string {
+    const totalSlides = this.recommendedDrinks().length;
+    const previousIndex = (this.currentSlideIdx() - 1 + totalSlides) % totalSlides;
+    const nextIndex = (this.currentSlideIdx() + 1) % totalSlides;
+    if (index === this.currentSlideIdx()) return 'focus';
+    if (index === previousIndex || index === nextIndex) return 'neighbour';
+    return 'hidden';
+  }
+
+  isPrevDisabled(): boolean {
+    return this.currentSlideIdx() === 0;
+  }
+  isNextDisabled(): boolean {
+    return this.currentSlideIdx() === this.recommendedDrinks().length - 1;
+  }
+
+  filterDrinksByQuery() {
     this.filteredDrinks.set(
-      this.D_allDrinks().filter(drink =>
+      this.allDrinks().filter(drink =>
         drink.name.toLowerCase().includes(this.searchQuery.toLowerCase())
       )
     );
   }
-  D_filterDrinks() {
+
+  filterDrinksByIngredients() {
     if (this.selectedIngredient) {
       this.filteredDrinks.set(
-        this.D_allDrinks().filter(drink =>
+        this.allDrinks().filter(drink =>
           drink.drinkIngredients.some(ing => ing.name === this.selectedIngredient)
         )
       );
     } else {
-      this.filteredDrinks.set(this.D_allDrinks());
+      this.filteredDrinks.set(this.allDrinks());
     }
   }
-  D_getUniqueIngredients(): string[] {
+
+  getUniqueIngredients(): string[] {
     const ingredientsSet = new Set<string>();
-    this.D_allDrinks().filter(d => d != null).forEach(drink => {
+    this.allDrinks().filter(d => d != null).forEach(drink => {
       drink.drinkIngredients.forEach(ing => ingredientsSet.add(ing.name));
     });
     return Array.from(ingredientsSet);
