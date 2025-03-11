d4d211ee1fda74d1e2e55a0fc50c6eca24e1c067
Moritz Bernhofer <114606708+MoritzBernhofer@users.noreply.github.com>
2025-03-11 03:59:45 +0100

    small fix

--- Backend/Services/PumpService/VPump.cs
+++ Backend/Services/PumpService/VPump.cs
@@ -1,40 +1,39 @@
-using System;
-using System.Device.Gpio;
 using System.Device.Pwm.Drivers;
 
-namespace Backend.Services.PumpService
+namespace Backend.Services.PumpService;
+
+public class VPump
 {
-    public class VPump
+    private const int Frequency = 20_000;
+
+    private readonly SoftwarePwmChannel _channel1;
+    private readonly GpioController _controller;
+
+
+    public VPump(int in1, int in2, GpioController controller)
     {
-        private const int Frequency = 20_000;
-        private readonly SoftwarePwmChannel _pwmChannel;
+        _controller = controller;
 
-        public VPump(int in1, int in2, GpioController controller)
-        {
-            _pwmChannel = new SoftwarePwmChannel(in1, Frequency, 0);
+        _channel1 = new SoftwarePwmChannel(in1, Frequency, 0);
 
-            controller.OpenPin(in1, PinMode.Output);
-            controller.OpenPin(in2, PinMode.Output);
+        controller.OpenPin(in2, PinMode.Output);
+        controller.Write(in2, PinValue.Low);
+    }
 
-            controller.Write(in2, PinValue.Low);
-        }
+    public void SetSpeed(int percentage)
+    {
+        if (percentage is < 0 or > 100) throw new Exception("percentage format -> between 0 - 100");
 
-        public void SetSpeed(int percentage)
-        {
-            if (percentage is < 0 or > 100)
-                throw new ArgumentOutOfRangeException(nameof(percentage), "Percentage must be between 0 and 100");
+        _channel1.DutyCycle = Math.Round(percentage / 100.0, 2);
+    }
 
-            _pwmChannel.DutyCycle = Math.Round(percentage / 100.0, 2);
-        }
+    public void Start()
+    {
+        _channel1.Start();
+    }
 
-        public void Start()
-        {
-            _pwmChannel.Start();
-        }
-
-        public void Stop()
-        {
-            _pwmChannel.Stop();
-        }
+    public void Stop()
+    {
+        _channel1.Stop();
     }
 }
