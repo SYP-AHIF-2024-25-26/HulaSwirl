181144cbb7ac36dfded9cec1011797c755ce7fc5
Lina <114607173+Caroline-Hornhuber@users.noreply.github.com>
2025-03-11 08:51:15 +0100

    -

--- Frontend/src/app/background-leaves/background-leaves.component.ts
+++ Frontend/src/app/background-leaves/background-leaves.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-background-leaves',
-  imports: [],
-  templateUrl: './background-leaves.component.html',
-  styleUrl: './background-leaves.component.css'
+    selector: 'app-background-leaves',
+    imports: [],
+    templateUrl: './background-leaves.component.html',
+    standalone: true,
+    styleUrl: './background-leaves.component.css'
 })
 export class BackgroundLeavesComponent {
 
